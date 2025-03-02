37eb7bedb3889a70d3c9544b8f00caf7e63d358e
Charan <cpanchavatig@nvidia.com>
2025-03-02 09:21:07 -0800

    clean up

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-source venv/bin/activate
-
-echo "Starting the FastAPI application with Uvicorn..."
-# Start the application in reload mode for development
-uvicorn app.main:app --reload
-
