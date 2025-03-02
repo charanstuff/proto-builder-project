1f9d719514fd370ba077eb1ecdfbac8dc63e4f5b
Charan <cpanchavatig@nvidia.com>
2025-03-02 09:56:16 -0800

    cleanup

--- proto-builder/agents/modification.py
+++
@@ -1,8 +0,0 @@
-from agents.developer import developer
-
-class ModificationHandler:
-    def request_changes(self, feature_request):
-        prompt = f"Modify the current prototype to include: {feature_request}"
-        return developer.generate(prompt)
-
-mod_handler = ModificationHandler()
